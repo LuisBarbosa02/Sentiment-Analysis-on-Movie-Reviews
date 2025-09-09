dafb87401096dec25ea3884649379290e8a94b2d
Luis <luiscastro250302@gmail.com>
2025-09-09 13:31:00 -0300

    Added a new dataset to train the model on.

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 # Import libraries
 import pandas as pd
 from pathlib import Path
+import os
 
 # Load data
 def load_data(path):
@@ -12,17 +13,30 @@ def load_data(path):
     """
     data_dir = Path(path)
 
-    rows = []
-    for label in ('pos', 'neg'):
-        folder = data_dir / label
-        for file in sorted(folder.glob('*.txt')):
-            text = file.read_text(encoding='utf8', errors='ignore')
+    if path == 'datasets/review_polarity':
+        rows = []
+        for label in ('pos', 'neg'):
+            folder = data_dir / label
+            for file in sorted(folder.glob('*.txt')):
+                text = file.read_text(encoding='utf8', errors='ignore')
 
-            tag = file.name.split("_", 1)[0]
-            idx = int(tag.replace('cv', ''))
-            fold = idx // 100+1
+                tag = file.name.split("_", 1)[0]
+                idx = int(tag.replace('cv', ''))
+                fold = idx // 100+1
 
-            rows.append({'text': text, 'label': 1 if label=='pos' else 0, 'fold': fold})
+                rows.append({'text': text, 'label': 1 if label=='pos' else 0, 'fold': fold})
 
-    dataset = pd.DataFrame(rows)
-    return dataset
+        return pd.DataFrame(rows)
+
+    if path == 'datasets/rt-polaritydata':
+        rows = []
+        for file_name in os.listdir(path):
+            with open(f"{path}/{file_name}", 'rb') as file:
+                content = file.read().decode('utf-8', errors='ignore').split('\n')
+                content = [{'text': text, 'label': 1 if 'pos' in file_name else 0} for text in content]
+                del content[-1]
+                rows += content
+        return pd.DataFrame(rows)
+
+if __name__ == '__main__':
+    pass
